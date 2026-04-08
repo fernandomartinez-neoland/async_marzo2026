c373d948f759b9c17270d52049807bb065d92c25
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-08 10:36:02 +0200

    leer promises

--- modulo.js
+++ modulo.js
@@ -1,8 +1,24 @@
 import { contador, alerta } from "./contador.js";
 import { promesa } from "./promesa.js";
 
+const async=document.getElementById("async")
 contador()
 // alerta()
 
-console.log(promesa(true))
+// las promesas no se leen directamente llamando a la funcion, ya que este daria una respuesta compleja que no es lo que buscamos
+
+// para leer correctamente una promesa e ir directo a la respuesta de la misma debemos usar la palabra reservada await (esperar)  esta se encarga de esperar la respuesta de la promesa y leerla correctamente
+await promesa(true)
+.then((response)=>{//con la promesa usamos el then para controlar las respuestas correctas y poder hacer uso de ellas segun lo que nos plazca
+    // el then funciona con una funcion de flecha que se encarga de leer las respuestas para luego poder hacer con eso lo que queramos
+    async.textContent=response
+    console.log(response)
+})
+.catch((e)=>{
+    // el .catch se encarga de leer los errores para asi poder controlarlo y hacer lo que necesitemos con dicho error
+    async.textContent=e
+    console.log(e)
+})
+;
+
 
--- promesa.js
+++ promesa.js
@@ -2,7 +2,7 @@ export function promesa(pepito) {
 // las promesas responden en resuelto o rechazado, la palabra reject y resolve no son reservadas pero por norma general se usan estas palabras en especifico cuandos se trabaja en equipo
     return new Promise(
         (resolve, reject) => {
-            if (pepito = true) {
+            if (pepito == true) {
                 resolve("todo bien")
             } else {
                 reject("todo mal")
